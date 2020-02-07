4b0de1d2aa251fdf24e460b30fdca9da6fb08d6f
Raul Lopez Martinez <raul.lopezm@stkrursus.com>
2020-02-07 12:40:48 -0600

    Se agrega el modelo de Areas en el front y se integra

--- src/app/proveedor/components/investigacion/investigacion.component.ts
+++ src/app/proveedor/components/investigacion/investigacion.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/common/service/data.service';
 import { Router } from '@angular/router';
+import { Area }  from 'src/app/common/models/area'
+
 
 @Component({
   selector: 'app-investigacion',
@@ -9,13 +11,22 @@ import { Router } from '@angular/router';
 })
 export class InvestigacionComponent implements OnInit {
 
+  // areas : Area[];
+  areas: Array<Area> = [
+    {id: '0', descripcion: 'Seleccione una opción'},
+    {id: '1', descripcion: 'Bienes'},
+    {id: '2', descripcion: 'Servicios'},
+    {id: '3', descripcion: 'Obras'},
+    {id: '4', descripcion: 'Servicios por obra'},
+];
+
   constructor(
     private data: DataService,
     private router: Router){
-      
     }
 
   ngOnInit() {
+
   }
 
   navegarCuestionario(){
